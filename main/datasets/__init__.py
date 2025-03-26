9a2dfb9b293841da8aecdda7d03812485d622fbe
Thanhpx <thanh.pham@gamifystudios.co>
2025-03-26 23:08:51 +0700

    add cifar100

--- main/datasets/__init__.py
+++ main/datasets/__init__.py
@@ -1,5 +1,6 @@
 from .celeba import CelebADataset
 from .cifar10 import CIFAR10Dataset
+from .cifar100 import CIFAR100Dataset
 from .celebahq import CelebAHQDataset
 from .afhq import AFHQv2Dataset
 from .inpaint import InpaintDataset
